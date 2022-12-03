a10abb498f592fc2b3e47c04a96acc6a0f7b7d3d
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2022-12-03 20:45:14 +0530

    Time: 71 ms (20.16%), Space: 19.2 MB (59.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        
+//         This code is also working---but more code
+        // int i,j;
+        // i=j=0;
+        
+        // while(j<nums.size())
+        // {
+        //     if(nums[i]==0 && nums[j]==0)
+        //     {
+        //         j++;
+        //     }
+        //     else if(nums[i]==0 && nums[j] != 0)
+        //     {
+        //         swap(nums[i],nums[j]);
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(nums[i] != 0 && nums[j] == 0)
+        //     {
+        //         i++;
+        //         j++;
+        //     }
+        //    else{
+        //        i++;
+        //        j++;
+        //    }
+        // }
+        
+//         Solution with less code
+       
         int i,j;
         i=j=0;
         
-        while(j<nums.size())
+        while(i<nums.size())
         {
-            if(nums[i]==0 && nums[j]==0)
+            if(nums[i] != 0)
             {
+                swap(nums[j],nums[i]);
                 j++;
             }
-            else if(nums[i]==0 && nums[j] != 0)
-            {
-                swap(nums[i],nums[j]);
-                i++;
-                j++;
-            }
-            else if(nums[i] != 0 && nums[j] == 0)
-            {
-                i++;
-                j++;
-            }
-           else{
-               i++;
-               j++;
-           }
+            i++;
         }
     }
 };
