e6c9341a00010860b138de485b1c5108f4cda7ae
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-01-14 14:52:18 +0530

    Added solution - LeetHub

--- Help Classmates - GFG/help-classmates.cpp
+++ Help Classmates - GFG/help-classmates.cpp
@@ -39,19 +39,13 @@ class Solution{
         st.push(-1);
         for(int i=n-1;i>=0;i--)
             {
-                if(st.top() < arr[i])
-                    {
-                        v.push_back(st.top());
-                        st.push(arr[i]);
-                    }
-                else {
                     while(st.top() >= arr[i])
                         {
                             st.pop();
                         }
                     v.push_back(st.top());
                     st.push(arr[i]);
-                }   
+                 
             }
             reverse(v.begin(),v.end());
             return v;
