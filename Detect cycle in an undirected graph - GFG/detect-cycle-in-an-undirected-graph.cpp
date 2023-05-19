1477af8c8b2b00a0f36b917904c03c46be81d365
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-05-19 11:56:05 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -4,49 +4,35 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    private:
-    bool detectCycle(vector<int> adj[],vector<int> &visited,queue<pair<int,int>> q,int node){
-
-        q.push({node,-1});
-        visited[node]=1;
-        while(!q.empty())
-            {
-                int node=q.front().first;
-                int parent=q.front().second;
-                q.pop();
-                for(auto adjacentNode: adj[node])
-                    {
-                        if(!visited[adjacentNode])
-                            {
-                                q.push({adjacentNode,node});
-                                visited[adjacentNode]=1;
-                            }
-                        else if(parent != adjacentNode)
-                            {
-                                return true;
-                            }
-                    }
-            }
-        return false;
-    }
+  private: 
+    bool detectCycle(vector<int> adj[],vector<int> &visited,int parent,int node)
+        {
+            visited[node]=1;
+            for(auto adjacentNode:adj[node])
+                {
+                    if(!visited[adjacentNode])
+                        {
+                            if(detectCycle(adj,visited,node,adjacentNode)) return true;
+                        }
+                    else if(parent != adjacentNode)
+                        return true;
+                }
+            return false;
+        }
   public:
     // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
         // Code here
         vector<int> visited(V,0);
-        queue<pair<int,int>> q;
-       
-        
+        int initialNodeParent=-1;
         for(int i=0;i<V;i++)
             {
                 if(!visited[i])
                     {
-                        if(detectCycle(adj,visited,q,i) == true)
-                            return true;
+                        if(detectCycle(adj,visited,initialNodeParent,i)) return true;
                     }
             }
-        return false;
-        
+            return false;
     }
 };
 
