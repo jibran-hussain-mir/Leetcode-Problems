1b998ce3398d2ce237f6361de69c6d5dd081c384
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-09-30 12:06:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (6.66%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int count(int m,int n,vector<vector<int>> &dp){
-        if(m == 0 && n == 0) return 1;
-        
-        if(m < 0 || n < 0 ) return 0;
-        
-        if(dp[m][n] != -1) return dp[m][n];
-        
-        int left=count(m,n-1,dp);
-        int up=count(m-1,n,dp);
-        return dp[m][n]=left + up;
-    }
-    
     int uniquePaths(int m, int n) {
-        int rowSize=m;
-        int colSize=n;
-    vector<vector<int>> dp(m,vector<int>(n,-1));
-        return count(m-1,n-1,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i == 0 && j == 0){
+                    dp[0][0]=1;
+                    continue;
+                }
+                 int left=0,up=0;
+                 if(j > 0) left= dp[i][j-1];
+                 if(i > 0)  up= dp[i-1][j];
+                dp[i][j] = left + up;
+            }
+        }
+        return dp[m-1][n-1];        
     }
 };
