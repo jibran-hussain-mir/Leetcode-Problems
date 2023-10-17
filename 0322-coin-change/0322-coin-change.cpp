785a96861ad3b9f4c26e11939933afb34225ea23
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-10-17 11:31:11 +0530

    Time: 88 ms (36.58%), Space: 45.9 MB (22.24%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    int minCoins(int index,int target,vector<int> &coins,vector<vector<int>>& dp){
+        if(index == 0){
+            if(target % coins[0] == 0) return target/coins[0];
+            else return 1e9;
+        }
+        
+        if(dp[index][target] != -1) return dp[index][target];
+        
+        int notTake=minCoins(index-1,target,coins,dp);
+        int take=INT_MAX;
+        if(coins[index] <= target){
+            take= 1  + minCoins(index,target-coins[index],coins,dp);
+        }
+        return dp[index][target]=min(take,notTake);
+    }
+    
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        int ans= minCoins(n-1,amount,coins,dp);
+        if(ans >= 1e9 ) return -1;
+        return ans;
+    }
+};
