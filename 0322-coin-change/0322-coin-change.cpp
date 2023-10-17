8b344487ee9cd92fbbd95a22b2c254da334aa888
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-10-17 12:30:48 +0530

    Time: 99 ms (29.51%), Space: 45.4 MB (32.10%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,28 @@
 class Solution {
-    int minCoins(int index,int target,vector<int> &coins,vector<vector<int>>& dp){
-        if(index == 0){
-            if(target % coins[0] == 0) return target/coins[0];
-            else return 1e9;
-        }
-        
-        if(dp[index][target] != -1) return dp[index][target];
-        
-        int notTake=minCoins(index-1,target,coins,dp);
-        int take=INT_MAX;
-        if(coins[index] <= target){
-            take= 1  + minCoins(index,target-coins[index],coins,dp);
-        }
-        return dp[index][target]=min(take,notTake);
-    }
+
     
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans= minCoins(n-1,amount,coins,dp);
-        if(ans >= 1e9 ) return -1;
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+//         Tabulation Code
+        for(int j=0;j<=amount;j++){
+            if(j % coins[0] == 0) dp[0][j]=j/coins[0];
+            else dp[0][j] =1e9;
+        }
+        
+        for(int index=1;index<n;index++){
+            for(int target=0;target<=amount;target++){
+                int notTake=dp[index-1][target];
+                int take= INT_MAX;
+                if(coins[index] <= target){
+                     take =1+ dp[index][target-coins[index]];
+              }
+                dp[index][target]=min(take,notTake);
+            }
+        }
+        int ans= dp[n-1][amount];
+         if(ans >= 1e9 ) return -1;
         return ans;
     }
 };
