976571df2af293d3d2fcea7c4e5a7c3a7e41b70b
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-10-17 12:36:30 +0530

    Time: 73 ms (59.20%), Space: 18.9 MB (39.70%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -4,24 +4,25 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        vector<int> pre(amount+1,0),curr(amount+1,0);
 //         Tabulation Code
         for(int j=0;j<=amount;j++){
-            if(j % coins[0] == 0) dp[0][j]=j/coins[0];
-            else dp[0][j] =1e9;
+            if(j % coins[0] == 0) pre[j]=j/coins[0];
+            else pre[j] =1e9;
         }
         
         for(int index=1;index<n;index++){
             for(int target=0;target<=amount;target++){
-                int notTake=dp[index-1][target];
+                int notTake=pre[target];
                 int take= INT_MAX;
                 if(coins[index] <= target){
-                     take =1+ dp[index][target-coins[index]];
+                     take =1+ curr[target-coins[index]];
               }
-                dp[index][target]=min(take,notTake);
+                curr[target]=min(take,notTake);
             }
+            pre=curr;
         }
-        int ans= dp[n-1][amount];
+        int ans= pre[amount];
          if(ans >= 1e9 ) return -1;
         return ans;
     }
