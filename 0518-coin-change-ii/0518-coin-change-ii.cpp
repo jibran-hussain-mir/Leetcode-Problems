3375195ef027d8e812dc7a1ac3554e6b56ceb72a
jibran-hussain-mir <84909335+jibran-hussain-mir@users.noreply.github.com>
2023-10-18 12:27:51 +0530

    Time: 35 ms (31.64%), Space: 18.5 MB (50.82%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,29 @@
-class Solution {    
+class Solution {
 public:
-    int maxWays(int index,int target,vector<int>& denominations,vector<vector<int>> &dp){
-    // Base Case
-    if(target == 0 ) return 1;
-    if(index == 0 ){
-        if(target % denominations[0] == 0) return 1;
-        return 0;
-    }
-    if(dp[index][target] != -1) return dp[index][target];
-    // Total Possibilities
-    int notTake = maxWays(index-1,target,denominations,dp);
-    int take = 0;
-    if(target >= denominations[index]) take = maxWays(index,target-denominations[index],denominations,dp);
-
-    return dp[index][target]=take + notTake;
-}
-    
     int change(int amount, vector<int>& coins) {
-         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        return maxWays(coins.size()-1,amount,coins,dp);
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
+
+        // Tabulation
+        for (int j = 0; j <= amount; j++) {
+            if (j % coins[0] == 0) 
+                dp[0][j] = 1;
+            else 
+                dp[0][j] = 0;
+        }
+
+        for (int index = 1; index < n; index++) {
+            for (int target = 0; target <= amount; target++) {
+                int notTake = dp[index - 1][target];
+                int take = 0;
+
+                if (target >= coins[index])
+                    take = dp[index][target - coins[index]];
+
+                dp[index][target] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][amount];
     }
 };
